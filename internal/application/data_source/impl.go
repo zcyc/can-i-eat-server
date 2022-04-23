74fd06d4bb835ea3d79ab10be8e67b7667e30bde
Charles <8595764@qq.com>
2022-04-23 20:11:14 +0800

    重构

--- internal/application/data_source/impl.go
+++ internal/application/data_source/impl.go
@@ -164,6 +164,81 @@ func (d dataSourceApplicationImpl) UploadBhJson(bhList common_domain.BhList) err
 		if r == '…' {
 			return nil
 		}
+		if r == 'α' {
+			return []string{"alpha"}
+		}
+		if r == 'β' {
+			return []string{"beta"}
+		}
+		if r == 'γ' {
+			return []string{"gamma"}
+		}
+		if r == 'δ' {
+			return []string{"delta"}
+		}
+		if r == 'ε' {
+			return []string{"epsilon"}
+		}
+		if r == 'ζ' {
+			return []string{"zeta"}
+		}
+		if r == 'η' {
+			return []string{"eta"}
+		}
+		if r == 'θ' {
+			return []string{"theta"}
+		}
+		if r == 'ι' {
+			return []string{"iota"}
+		}
+		if r == 'κ' {
+			return []string{"kappa"}
+		}
+		if r == 'λ' {
+			return []string{"lambda"}
+		}
+		if r == 'μ' {
+			return []string{"mu"}
+		}
+		if r == 'ν' {
+			return []string{"nu"}
+		}
+		if r == 'ξ' {
+			return []string{"xi"}
+		}
+		if r == 'ο' {
+			return []string{"omicron"}
+		}
+		if r == 'π' {
+			return []string{"pi"}
+		}
+		if r == 'ρ' {
+			return []string{"rho"}
+		}
+		if r == 'σ' {
+			return []string{"sigma"}
+		}
+		if r == 'ς' {
+			return []string{"sigma"}
+		}
+		if r == 'τ' {
+			return []string{"tau"}
+		}
+		if r == 'υ' {
+			return []string{"upsilon"}
+		}
+		if r == 'φ' {
+			return []string{"phi"}
+		}
+		if r == 'χ' {
+			return []string{"chi"}
+		}
+		if r == 'ψ' {
+			return []string{"psi"}
+		}
+		if r == 'ω' {
+			return []string{"omega"}
+		}
 		return []string{string(r)}
 	}
 
